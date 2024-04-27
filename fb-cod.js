47b5b9104f0fc18290c2ac944b928a0f78ce7e92
Omranalr <168029210+Omranalr@users.noreply.github.com>
2024-04-27 05:39:43 +0200

    Update fb-cod.js

--- fb-cod.js
+++ fb-cod.js
@@ -23,7 +23,7 @@ const auth = getAuth(app);
 const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://www.example.com/verification.html',
+ https:'//omranalr.github.io/Workers2/verification.htm',
     // This must be true.
     handleCodeInApp: true
 };
