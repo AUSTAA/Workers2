56d5a5cf3b15e080bc640f0cc8833f038642f29a
Omranalr <168029210+Omranalr@users.noreply.github.com>
2024-04-27 19:56:45 +0200

    Update firebaseri-registeration.js

--- firebaseri-registeration.js
+++ firebaseri-registeration.js
@@ -1,3 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA1L9YC72fzgG_UhbZJAyuueAePoM9CgAU",
+    authDomain: "workers2-e6d6f.firebaseapp.com",
+    projectId: "workers2-e6d6f",
+    storageBucket: "workers2-e6d6f.appspot.com",
+    messagingSenderId: "196462535548",
+    appId: "1:196462535548:web:e5ccd41d6adf5f893fbaf9",
+    measurementId: "G-EVVLM3Q526"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+// Define action code settings
+const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: 'https://omranalr.github.io/Workers2/verification.htm',
+    // This must be true.
+    handleCodeInApp: true
+};
+
 // Listen for the registration form submission event
 const registrationForm = document.getElementById('registration_form');
 registrationForm.addEventListener('submit', function(event) {
@@ -11,7 +41,7 @@ registrationForm.addEventListener('submit', function(event) {
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Send verification code via email
-            auth.currentUser.sendEmailVerification(actionCodeSettings)
+auth.currentUser.sendEmailVerification(actionCodeSettings)
                 .then(() => {
                     // Email sent
                     console.log("Verification email sent.");
