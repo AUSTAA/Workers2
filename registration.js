f963441f0e1f0c5d411aecd593cab9020e8e5eb6
Omranalr <168029210+Omranalr@users.noreply.github.com>
2024-05-11 19:55:27 +0200

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB7YJhtaefEPc9NMzhTBjQC06WmSEja0xc",
-  authDomain: "omran-16f44.firebaseapp.com",
-  databaseURL: "https://omran-16f44-default-rtdb.firebaseio.com",
-  projectId: "omran-16f44",
-  storageBucket: "omran-16f44.appspot.com",
-  messagingSenderId: "598982209417",
-  appId: "1:598982209417:web:dc9cbddd485a1ea52bbb58",
-  measurementId: "G-
-    });
+    apiKey: "AIzaSyB7YJhtaefEPc9NMzhTBjQC06WmSEja0xc",
+    authDomain: "omran-16f44.firebaseapp.com",
+    databaseURL: "https://omran-16f44-default-rtdb.firebaseio.com",
+    projectId: "omran-16f44",
+    storageBucket: "omran-16f44.appspot.com",
+    messagingSenderId: "598982209417",
+    appId: "1:598982209417:web:dc9cbddd485a1ea52bbb58",
+    measurementId: "G-PGZJ0T555G"
+  };
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 
