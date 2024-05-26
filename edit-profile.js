f5ac76fc0e5a6f179834ebbf222188e06ee824aa
Omranalr <168029210+Omranalr@users.noreply.github.com>
2024-05-26 02:42:31 +0200

    Update edit-profile.js

--- edit-profile.js
+++ edit-profile.js
@@ -25,7 +25,7 @@ auth.onAuthStateChanged((user) => {
             if (doc.exists) {
                 const userData = doc.data();
                 document.getElementById('username').value = userData.username || '';
-                document.getElementById('phoneNumber').value = userData.phoneNumber || '';
+                document.getElementById('phone').value = userData.phone || '';
                 document.getElementById('nationality').value = userData.nationality || '';
                 document.getElementById('city').value = userData.city || '';
                 document.getElementById('experienceYears').value = userData.experienceYears || '';
@@ -48,7 +48,6 @@ document.getElementById('profileForm').addEventListener('submit', async (e) => {
     e.preventDefault();
     
     const username = document.getElementById('username').value;
-    const Phone = document.getElementById('phoneNumber').value;
     const newPhone = document.getElementById('newPhone').value;
     const nationality = document.getElementById('nationality').value;
     const city = document.getElementById('city').value;
@@ -63,7 +62,6 @@ document.getElementById('profileForm').addEventListener('submit', async (e) => {
 
         const updatedData = {
             username,
-            phoneNumber,
             nationality,
             city,
             experienceYears,
